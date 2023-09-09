8dc7e2cd5c23d4a904f784cd69788c7c7f0c4e36
Kilativ54 <vitalikechkalov@gmail.com>
2023-09-09 22:53:21 +0200

    change contacts to fix filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,11 +12,9 @@ export const App = () => {
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ];
-
+  const storage = () => JSON.parse(window.localStorage.getItem('contacts'));
+  const [contacts, setContacts] = useState(storage ?? contactArray);
   const [filters, setFilter] = useState('');
-
-  const storage = () => JSON.parse(localStorage.getItem('contacts'));
-  const [contacts, setContacts] = useState(storage || contactArray);
 
   const handleSubmit = contactItem => {
     const { name } = contactItem;
